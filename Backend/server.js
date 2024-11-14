d40d1cf183a452d251106e1fd58fb1dac143ddd0
Adarsha <learnadarsha@gmail.com>
2024-11-14 01:08:04 -0500

    Add mongoose database to the project with schema for user and notes

--- Backend/server.js
+++ Backend/server.js
@@ -6,12 +6,17 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const corsOptions = require("./config/corsOptions")
 const {logger} = require("./middleware/logger")
+const {logEvents} = require("./middleware/logger")
 const { errorHandler } = require('./middleware/errorHandler')
 const { log } = require('console')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3500
 
 console.log(process.env.NODE_ENV)
+
+connectDB()
 
 app.use(cors(corsOptions))
 
@@ -37,6 +42,13 @@ app.all('*', (req, res) => {
 })
 
 app.use(errorHandler)
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
+mongoose.connection.once('open', () => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
 
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
